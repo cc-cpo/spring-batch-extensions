b1b5866f9c9d847d6c0e6e4e20a2d51024057d22
stefan <stefan.ostermayr@media-it.de>
2015-07-01 17:02:26 +0200

    tests

--- src/test/java/de/mediait/batch/FlatFileItemReaderTest.java
+++ src/test/java/de/mediait/batch/FlatFileItemReaderTest.java
@@ -6,7 +6,6 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.ArrayFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
-import org.springframework.batch.item.file.separator.RecordSeparatorPolicy;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.core.io.ClassPathResource;
 
@@ -18,7 +17,7 @@ public class FlatFileItemReaderTest {
 	@Test
 	public void testFlatFileReader() throws Exception {
 		
-		final FlatFileItemReader<String[]> reader = createFlatFileReader();
+		final FlatFileItemReader<String[]> reader = createFlatFileReader(';','\'');
 		
 		reader.setResource(new ClassPathResource("csv-fix-samples/linetofix.txt"));
 		
@@ -30,16 +29,35 @@ public class FlatFileItemReaderTest {
 
 		assertArrayEquals(new String[]{"begin","abc' \"d\" 'ef","end"}, object);
 	}
+	
+	
+	@Test
+	public void testFlatFileReader2() throws Exception {
+		
+		final FlatFileItemReader<String[]> reader = createFlatFileReader(',','\'');
+		
+		reader.setResource(new ClassPathResource("csv-fix-samples/linetofix2.txt"));
+		
+		final ExecutionContext executionContext = new ExecutionContext();
+		reader.open(executionContext);
+		final String[] object = (String[]) reader.read();
+		reader.close();
+
+
+		assertArrayEquals(new String[]{"begin","abc' \"d\" 'ef","end"}, object);
+	}
 
 	
-	private FlatFileItemReader<String[]> createFlatFileReader() {
+
+	
+	private FlatFileItemReader<String[]> createFlatFileReader(final char seperatorCharacter, final char quoteCharacter) {
 		final FlatFileItemReader<String[]> reader = new FlatFileItemReader<String[]>();
 		final DefaultLineMapper<String[]> lineMapper = new DefaultLineMapper<String[]>();
-		final DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(";");
-		tokenizer.setQuoteCharacter('\'');
+		final DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(String.valueOf(seperatorCharacter));
+		tokenizer.setQuoteCharacter(quoteCharacter);
 		lineMapper.setLineTokenizer(tokenizer);
 		final FixingCsvRecordSeparatorPolicy recordSeparatorPolicy = new FixingCsvRecordSeparatorPolicy();
-		recordSeparatorPolicy.setQuoteCharacter('\'');
+		recordSeparatorPolicy.setQuoteCharacter(quoteCharacter);
 		reader.setRecordSeparatorPolicy(recordSeparatorPolicy);
 		reader.setLineMapper(lineMapper);
 		final FieldSetMapper<String[]> fieldSetMapper = new ArrayFieldSetMapper();
